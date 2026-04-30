87cd35d16fb85e86fc815a9daf161d5223acd7a3
perandowind <perandowind@gmail.com>
2026-04-30 11:08:26 +0900

    step 1

--- src/main/kotlin/wiseSaying/Main.kt
+++ src/main/kotlin/wiseSaying/Main.kt
@@ -1,5 +1,20 @@
 package wiseSaying
 
 fun main() {
-    print("hello")
+
+    println("== 명언 앱 ==")
+
+    while (true) {
+        print("명령) ")
+
+        val input = readln()
+        when (input) {
+            "종료" ->{
+                println("프로그램을 종료합니다.")
+                return
+            }
+
+        }
+    }
+
 }
