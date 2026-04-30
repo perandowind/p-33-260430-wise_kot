355d001c10c1342eaea4a17143afbce63cd24fd9
perandowind <perandowind@gmail.com>
2026-04-30 11:31:06 +0900

    6

--- src/main/kotlin/wiseSaying/App.kt
+++ src/main/kotlin/wiseSaying/App.kt
@@ -4,6 +4,8 @@ class App {
 
     fun run() {
         var lastId = 0
+        val wiseSayings = mutableListOf<WiseSaying>()
+
         println("== 명언 앱 ==")
 
         while (true) {
@@ -15,6 +17,7 @@ class App {
                     println("프로그램을 종료합니다.")
                     return
                 }
+
                 "등록" -> {
                     print("명언): ")
                     val content = readln()
@@ -22,7 +25,18 @@ class App {
                     val author = readln()
                     val id = ++lastId
 
+                    WiseSaying(id, content, author)
+                        .also { wiseSayings.add(it) }
+
                     println("${id}번 명언이 등록되었습니다.")
+                }
+
+                "목록" -> {
+                    println("번호 / 작가 / 명언")
+                    println("-".repeat(30))
+                    wiseSayings.reversed().forEach {
+                        println("${it.id} / ${it.author} / ${it.content}")
+                    }
                 }
 
             }
